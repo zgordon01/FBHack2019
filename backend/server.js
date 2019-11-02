5a44870161343aab7c43be01422d9804810f8fa1
Arianna Osar <ariannaosar@gmail.com>
2019-11-02 15:42:31 -0500

    firebase cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const functions = require('firebase-functions');
 const {getAllPageActivity: getAllPageActiivty, receiveHook} = require('./requestHelpers.js');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
 
 // processing functions
 const {getAllPageActivity, receiveHookPost, receiveHookGet} = require('./requestHelpers.js');
@@ -35,5 +35,4 @@ app.post('/receive', receiveHookPost);
 app.get('/receive', receiveHookGet);
 
 
-exports.app = functions.https.onRequest(app);
-
+app.listen(port, () => console.log(`Listening on port ${port}`));
