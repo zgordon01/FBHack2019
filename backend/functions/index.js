bbce59a05f596e2fbad098226ac928bdd032673d
Arianna Osar <aosar@lemon.localdomain>
2019-11-02 11:30:43 -0500

    Add page activity endpoint to firebase-served file

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,11 +1,13 @@
 const functions = require('firebase-functions');
 
+const {getAllPageActivity: getAllPageActiivty} = require('../requestHelpers.js');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.timeout = 0;
 
+app.get('/get-page-activity', getAllPageActiivty);
 app.get('/hello', (req, res, next)=>{
     return res.json({
         message: 'hello world'
