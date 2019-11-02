93656b46c43364b1683b5d0449e67cf66c017011
Zach <zachary.gordon@gmail.com>
2019-11-02 15:26:11 -0500

    committing all changes in functions

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,9 +1,10 @@
 const functions = require('firebase-functions');
+const {getAllPageActivity: getAllPageActiivty, receiveHook} = require('./requestHelpers.js');
 const express = require('express');
 const app = express();
 
 // processing functions
-const {getAllPageActivity, receiveHook} = require('./requestHelpers.js');
+const {getAllPageActivity, receiveHookPost, receiveHookGet} = require('./requestHelpers.js');
 const {parsePostData} = require('./filters.js');
 
 const asyncMiddleware = fn => (req, res, next) => {
@@ -13,7 +14,7 @@ const asyncMiddleware = fn => (req, res, next) => {
 
 app.timeout = 0;
 
-app.get('/get-page-posts', asyncMiddleware( 
+app.get('/get-page-posts', asyncMiddleware(
   async (req, res, next) => {
     try{
       let data = await getAllPageActivity();
@@ -30,8 +31,8 @@ app.get('/hello', (req, res, next)=>{
   });
 });
 
-app.post('/receive', receiveHook);
-app.get('/receive', receiveHook);
+app.post('/receive', receiveHookPost);
+app.get('/receive', receiveHookGet);
 
 
 exports.app = functions.https.onRequest(app);
--- backend/functions/requestHelpers.js
+++ backend/functions/requestHelpers.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const auth = require('./config.json').access_token;
 const fetch = require('node-fetch');
 const headers = {
@@ -6,45 +7,62 @@ const headers = {
 };
 
 const getAllPageActivity = async () => {
-  const url = 'https://graph.facebook.com/v3.2/103256931121230/feed';
-  return await fetch(url, {
-    method: 'get',
-    headers
-  });
-}
+    const url = 'https://graph.facebook.com/v3.2/103256931121230/feed';
+    return await fetch(url, {
+        method: 'get',
+        headers
+    });
+};
 
-const receiveHook = (req, res) => {
+const postToPage = async (message) => {
+    const url = `https://graph.facebook.com/v3.2/103256931121230/feed?message=${message}`;
 
-  console.log(req.query);
+    return await fetch(url, {
+        method: 'POST',
+        headers
+    });
+};
 
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "FB_COMMUNITY";
+const receiveHookPost = (req, res) => {
+    console.log('entries returns', Object.keys(req.body));
 
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-  console.log('inside of receive');
-  console.log('verify token', token);
-  console.log('mode', mode);
-  console.log(req.query);
-  console.log('senderID', entries);
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
+    postToPage('skunkisherefromcode').then(() => {
+            res.sendStatus(200);
+        }, (err) => {
+            console.warn(err);
+            res.sendStatus(200);
+        }
+    );
+};
 
-    // Checks the mode and token sent is correct
-    if (token === VERIFY_TOKEN) {
+const receiveHookGet = (req, res) => {
 
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
+    const userId = _.get(req, 'query.entry.messaging.sender.id');
 
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "FB_COMMUNITY";
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+        res.sendStatus(200);
     }
-  }
-  res.sendStatus(200).send({readMe: 'helloWorld'});
 }
 
-module.exports = {getAllPageActivity, receiveHook}
+module.exports = {getAllPageActivity, receiveHookPost, receiveHookGet}
